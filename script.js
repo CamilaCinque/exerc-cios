c2525b2547cc4037e031c81ae0f1e105383b0773
CAMILA CINQUE DO NASCIMENTO <52643663810@info.local>
2026-08-26 17:45:34 -0300

    atualizacao

--- script.js
+++ script.js
@@ -2,22 +2,21 @@
 
 Crie um programa que solicite a idade de uma pessoa e informe se ela é maior de idade (18 anos ou mais) ou menor de idade. */
 
-/* let idade = Number(prompt("Informe a sua idade:"))
+let idade01 = Number(prompt("Informe a sua idade:"))
 
-if (Number.isNaN(idade)) {
+if (Number.isNaN(idade01)) {
     alert("Idade inválida. Informe somente números.")
 
-} else if (idade <18) {
+} else if (idade01 <18) {
     alert("Você é menor de idade.")
 } else {
     alert("Você é maior de idade.")
-} */
+}
 
 /* 2. Positivo ou Negativo
 
 Solicite um número ao usuário e informe se ele é positivo ou negativo. Considere o número 0 como negativo. */
 
-/*
 let numero = Number(prompt("Informe um número:"))
 
 if (Number.isNaN(numero)) {
@@ -27,12 +26,12 @@ if (Number.isNaN(numero)) {
     alert("Este número é negativo.")
 } else {
     alert("Este número é positivo.")
-} */
+}
 
 /* 3. Aprovação em uma prova
 
 Peça ao usuário a nota final de um aluno (de 0 a 100). Caso a nota seja 60 ou maior, informe que ele foi aprovado. Caso contrário, informe que foi reprovado. */
-/*
+
 let nota = Number(prompt("Informe a nota do aluno:"))
 
 if (Number.isNaN(nota)) {
@@ -45,15 +44,12 @@ if (Number.isNaN(nota)) {
     alert("O aluno foi aprovado.")
 } else {
     alert("Inválido. Informe um valor entre 0 e 100.")
-} */
+}
 
 /* 4. Positivo, Negativo ou Zero
 
-Leia um número informado pelo usuário e indique se ele é:
-Positivo; 
-Negativo; 
-Zero. */
-/*
+Leia um número informado pelo usuário e indique se ele é: positivo, negativo ou zero. */
+
 let numero01 = Number(prompt("Informe um número:"))
 
 if (Number.isNaN(numero01)) {
@@ -65,17 +61,13 @@ if (Number.isNaN(numero01)) {
 } else {
     alert("Este número é positivo.")
 }
-*/
 
 /* 5. Classificação por idade
 
-Solicite a idade de uma pessoa e classifique-a conforme as regras abaixo:
-Criança: de 0 a 12 anos; 
-Adolescente: de 13 a 17 anos; 
-Adulto: 18 anos ou mais. 
+Solicite a idade de uma pessoa e classifique-a conforme as regras abaixo: criança: de 0 a 12 anos; adolescente: de 13 a 17 anos; adulto: 18 anos ou mais. 
 
 Caso seja informada uma idade negativa, exiba uma mensagem de idade inválida. */
-/*
+
 let idade = Number(prompt("Informe a idade:"))
 
 if (Number.isNaN(idade)) {
@@ -87,12 +79,11 @@ if (Number.isNaN(idade)) {
 } else {
     alert("É um adulto.")
 }
-*/
 
 /* 6. Par ou Ímpar
 
 Solicite um número inteiro ao usuário e informe se ele é par ou ímpar. */
-/*
+
 let numero02 = Number(prompt("Informe um número:"))
 let resultado = numero02 % 2
 
@@ -101,16 +92,13 @@ if (resultado === 0) {
 } else {
     alert("Número ímpar!") 
 }
-*/
 
 /* 1. Calculadora simples
-Crie uma calculadora que:
-Solicite o primeiro número; 
-Solicite a operação desejada (+, -, * ou /); 
-Solicite o segundo número. 
+
+Crie uma calculadora que: solicite o primeiro número; solicite a operação desejada (+, -, * ou /); solicite o segundo número. 
+
 Ao final, exiba o resultado da operação escolhida. Caso seja informada uma operação inválida, exiba uma mensagem de erro. */
 
-/*
 let numero03 = Number(prompt("Informe o primeiro número:"))
 let operação = prompt ("Informe a operação desejada (+, -, * ou /)")
 let numero04 = Number(prompt("Informe o segundo número:"))
@@ -132,13 +120,11 @@ if (Number.isNaN(numero03)) {
     alert("O resultado é: " + (numero03 / numero04))
 } else {
     alert("Operação inválida. Escolha uma das operações a seguir: +, -, *, ou /.")
-} */
+} 
 
 /* 2. Maior entre três números
 
 Solicite três números diferentes ao usuário e informe qual deles é o maior. */
-
-/*
 
 let numero05 = Number(prompt("Informe o primeiro número:"))
 let numero06 = Number(prompt("Informe o segundo número:"))
@@ -154,14 +140,13 @@ if (Number.isNaN(numero05, numero06, numero07)) {
     alert("O segundo número é maior.")
 } else {
     alert("O terceiro número é maior.")
-} */
+}
 
 /* 3. Desconto em compras
-Uma loja oferece 10% de desconto para compras acima de R$ 100,00.
-Solicite o valor da compra e informe o valor final que deverá ser pago.
+Uma loja oferece 10% de desconto para compras acima de R$ 100,00. Solicite o valor da compra e informe o valor final que deverá ser pago.
+
 Caso a compra não atinja esse valor, informe o preço sem desconto. */
 
-/*
 let valor = Number(prompt("Informe o valor total da compra:"))
 let desconto = valor - (valor * 0.1)
 
@@ -171,15 +156,14 @@ if (Number.isNaN(valor)) {
     alert("Valor mínimo não atendido.")
 } else {
     alert("O valor da compra com desconto é: R$" + desconto + ".")
-} */
+}
 
 /* 4. Sistema de login
-Crie um programa que solicite um nome de usuário e uma senha.
-Considere que:
-Usuário: admin 
-Senha: 1234 
+
+Crie um programa que solicite um nome de usuário e uma senha. Considere que: usuário - admin | senha - 1234 .
+
 Caso ambos estejam corretos, exiba a mensagem "Login bem-sucedido". Caso contrário, exiba "Acesso negado". */
- /*
+
 const usuario = prompt("Informe o usuário:")
 const senha = prompt("Informe a senha:")
 
@@ -189,4 +173,4 @@ if (acessoAdmin) {
     alert("Acesso liberado!")
 } else {
     alert("Acesso negado!")
-} */
+}
